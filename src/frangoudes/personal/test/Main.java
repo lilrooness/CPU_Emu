f6036a15289d860a34af9f44a0bded5ae360a387
lilrooness <jfrangoudes@gmail.com>
2012-05-02 23:37:42 +0100

    Code Clean
    Deleted useless ask package with Assem.java class
    The original purpose for this class was to act as an assembler but a
    decision was made to use perl for this as
    the scripting language has already been used in the
    JASP emulator for an assembler and that seemed to work well.

--- src/frangoudes/personal/test/Main.java
+++ src/frangoudes/personal/test/Main.java
@@ -1,20 +1,10 @@
 package frangoudes.personal.test;
 
-import frangoudes.personal.asm.Assem;
-
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		//String[] program = {IS.MOV_A+"12", IS.MOV_B_A, IS.MOV_A+"00", "F000"};
-		String[] assem = {"mov a $10", "mov b a"};
-		Assem as = new Assem(assem);
-		
-		for(int i=0; i<assem.length; i++){
-			System.out.println(as.assembleLine());
-		}
 	}
 }
